191252cd32b8bd1ce27b93d853f36f8b142a4a57
LJS22 <LJS22@users.noreply.github.com>
2020-08-09 12:03:23 +0100

    Updates

---
+++ precache-manifest.cfaf7d3b7a70ecfe025fc27a3043f93a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da06543d240f455ae64219995deadc90",
+    "url": "/LJStobbartPortfolio/index.html"
+  },
+  {
+    "revision": "93b488fac4b62be5eb05",
+    "url": "/LJStobbartPortfolio/static/css/main.fb93f6aa.chunk.css"
+  },
+  {
+    "revision": "4210043ec73699a64b9f",
+    "url": "/LJStobbartPortfolio/static/js/2.69d51dd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LJStobbartPortfolio/static/js/2.69d51dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b488fac4b62be5eb05",
+    "url": "/LJStobbartPortfolio/static/js/main.42441604.chunk.js"
+  },
+  {
+    "revision": "5d29a4cfeb99767ecfd5",
+    "url": "/LJStobbartPortfolio/static/js/runtime-main.ac966051.js"
+  },
+  {
+    "revision": "a0e3593238694db45b80ec2a4789f66d",
+    "url": "/LJStobbartPortfolio/static/media/Arrow.a0e35932.png"
+  },
+  {
+    "revision": "ff7d8d97e14464af27169aacf237cbc1",
+    "url": "/LJStobbartPortfolio/static/media/BMI.ff7d8d97.png"
+  },
+  {
+    "revision": "cf627ced09590d77d4a59d3a5db2707a",
+    "url": "/LJStobbartPortfolio/static/media/Calculator.cf627ced.jpg"
+  },
+  {
+    "revision": "b0f64c72335ac13a78d9ab273e115f69",
+    "url": "/LJStobbartPortfolio/static/media/ContactsIcon.b0f64c72.jpg"
+  },
+  {
+    "revision": "20e7c654b449105f3fcc1110a0289366",
+    "url": "/LJStobbartPortfolio/static/media/Dice.20e7c654.png"
+  },
+  {
+    "revision": "03c41356a11e144e924643cee2ef4465",
+    "url": "/LJStobbartPortfolio/static/media/Drum.03c41356.png"
+  },
+  {
+    "revision": "a7e1275b9bf2ad2abf8a12fc193f7f1d",
+    "url": "/LJStobbartPortfolio/static/media/EmailIcon.a7e1275b.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/LJStobbartPortfolio/static/media/Github.3e54ed15.png"
+  },
+  {
+    "revision": "94ae2dcd4fa410811cab4e1fbb403340",
+    "url": "/LJStobbartPortfolio/static/media/JSImage.94ae2dcd.png"
+  },
+  {
+    "revision": "cf84af19a17162178a96190b8f593be0",
+    "url": "/LJStobbartPortfolio/static/media/MeNoBG.cf84af19.png"
+  },
+  {
+    "revision": "b10b140c6a5e314a0eccf161c5d4f112",
+    "url": "/LJStobbartPortfolio/static/media/MongoDB.b10b140c.png"
+  },
+  {
+    "revision": "67bce85dfa17ca93f66e1927d98444ae",
+    "url": "/LJStobbartPortfolio/static/media/MyLogo.67bce85d.jpg"
+  },
+  {
+    "revision": "854ac896af0919189fbac8eedd3ec98a",
+    "url": "/LJStobbartPortfolio/static/media/NodeJS.854ac896.png"
+  },
+  {
+    "revision": "2a8b77d3212d5813a9a1b2fac05892f3",
+    "url": "/LJStobbartPortfolio/static/media/ProjectsIcon.2a8b77d3.jpg"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/LJStobbartPortfolio/static/media/ReactLogo.c3ba82a5.png"
+  },
+  {
+    "revision": "f8eda46a4c33efcd673024f15ca5858a",
+    "url": "/LJStobbartPortfolio/static/media/Surman.f8eda46a.png"
+  },
+  {
+    "revision": "080d0fda628d14d4d3f14992c44dad02",
+    "url": "/LJStobbartPortfolio/static/media/ToDo.080d0fda.png"
+  },
+  {
+    "revision": "c3ce0c15c4fa916c837e84474bf143d1",
+    "url": "/LJStobbartPortfolio/static/media/cssImage.c3ce0c15.png"
+  },
+  {
+    "revision": "0ea316dbe0fd5dab21e160b3f48fa6b5",
+    "url": "/LJStobbartPortfolio/static/media/html.0ea316db.png"
+  }
+]);
